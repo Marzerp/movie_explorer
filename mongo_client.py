3644a973bef09c13d9a5a03901e4b9ac219c4e54
Marzerp <araceliromerozerpa@gmail.com>
2025-04-28 23:42:54 -0600

    MOV-15: Conection to mongo db

---
+++ mongo_client.py
@@ -0,0 +1,12 @@
+from pymongo import MongoClient
+import os
+
+def get_mongo_client():
+    username = os.getenv("MONGO_ROOT_USER")
+    password = os.getenv("MONGO_ROOT_PASSWORD")
+    db_name = os.getenv("MONGO_APP_DB")
+    host = os.getenv("MONGO_HOST")
+    port = os.getenv("MONGO_PORT", "27017")
+
+    client = MongoClient(f"mongodb://{username}:{password}@{host}:{port}/{db_name}")
+    return client[db_name]
