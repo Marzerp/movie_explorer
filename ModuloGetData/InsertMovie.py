eb6f8feb86c3aa80f9a355e7b66338e6d38d15ce
Marzerp <araceliromerozerpa@gmail.com>
2025-04-30 16:42:27 -0600

    MOV-15: Updated files

--- ModuloGetData/InsertMovie.py
+++ ModuloGetData/InsertMovie.py
@@ -3,29 +3,24 @@ from pymongo import MongoClient
 from .GetReviews import get_reviews
 from .mongo_client import get_mongo_client
 
-genres = ['Acción',
- 'Aventura',
- 'Animación',
- 'Comedia',
- 'Crimen',
- 'Documental',
- 'Drama',
- 'Familia',
- 'Fantasía',
- 'Historia',
- 'Terror',
- 'Música',
- 'Misterio',
- 'Romance',
- 'Ciencia ficción',
- 'Película de TV',
- 'Suspense',
- 'Bélica',
- 'Western']
+load_dotenv()
 
-emotions = 
+client= MongoClient(
+	f"mongodb://{os.getenv('MONGO_ROOT_USER')}:{os.getenv('MONGO_ROOT_PASSWORD')}" 
+ 	f"@{os.getenv('MONGO_HOST', 'localhost')}:27017/"
+ 	f"{os.getenv('DB_NAME', 'moviesdb')}
 
-def InsertReview(MoviesCollection:object):
+db = client[os.getenv("DB_NAME", "moviesdb")]
+reviews_collection = db.reviews
+
+
+
+def insert_reviews():
     '''Insert movie and their corresponding emotion 
        into a MongoDB collection'''
-    for review in GetReviews()
+    for review in get_reviews():
+    	reviews_collection.insert_one(review)
+    client.close()
+    
+
+
--- ModuloGetData/__main__.py
+++ ModuloGetData/__main__.py
@@ -1,5 +1,9 @@
 from .GetReviews import get_reviews
+from .InsertMovie import insert_reviews
 
 if __name__ == "__main__":
     reviews = get_reviews()
     print(reviews)
+
+if __name__ == "__main__":
+    insert_reviews()
